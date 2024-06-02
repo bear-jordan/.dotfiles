6b44b3bb749138061d48aa8eb3b2e6974214adc0
bear-jordan <bearjordan@gmail.com>
2024-06-02 10:34:23 -0400

    ENH: updated indenting in nvim

--- .config/nvim/lua/vim-config.lua
+++ .config/nvim/lua/vim-config.lua
@@ -1,11 +1,12 @@
+vim.cmd("set autoindent")
+vim.cmd("set expandtab")
 vim.cmd("set number")
 vim.cmd("set relativenumber")
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
-vim.cmd("set shiftwidth=4")
 vim.cmd("set scrolloff=8")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set smartindent")
+vim.cmd("set softtabstop=4")
+vim.cmd("set tabstop=4")
 vim.g.mapleader = " "
 vim.api.nvim_set_option("clipboard", "unnamed")
 vim.opt.undofile = true
